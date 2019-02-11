eaaf0f1ade62a510b24b2ffd67025d0af4179630
adamjc <adamjc86@gmail.com>
2019-02-11 20:27:46 +0000

    Update webpack to build the correct files

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,9 @@
 const path = require('path')
 
 module.exports = {
-  entry: './main.js',
+  entry: './src/main.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'main.js'
+    filename: './main.js'
   }
 }
