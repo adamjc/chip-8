4ae1dfa28759638a73d33ddbf89d1e59f01ffa6b
adamjc <adamjc86@gmail.com>
2019-02-13 22:55:56 +0000

    Add broken implementation of screen drawing

--- src/chip-8.js
+++ src/chip-8.js
@@ -5,6 +5,7 @@ const logger = new loggerFactory(true)
 // Made with a loooot of help from this excellent resource: http://devernay.free.fr/hacks/chip8/C8TECH10.HTM
 
 // CHIP-8 Interpreter
+const WORD_SIZE = 8
 
 // It has 16 8-bit data registers. V[0xF] is the carry register.
 let vRegisters = new Array(16)
@@ -31,45 +32,297 @@ let sp = new Array(16)
 
 // It utilises a 64x32 pixel display... we will get the chip-8 to write to these values, and in our `emulator` code, we will write these
 // values to a screen. Simple! (should be!).
-let display = new Array(64 * 32)
+const DISPLAY_WIDTH = 64
+const DISPLAY_HEIGHT = 32
+let display = new Array(DISPLAY_WIDTH).fill().map(_ => new Array(DISPLAY_HEIGHT).fill(0))
 
 // does a cpu cycle innit.
 function cycle () {
-  // fetching from memory takes twoooo cycles, cos it's an 8-bit bus, but each instruction is 16-bits long. Neat-o. VLIW are lame.
-  const inst = (memory[pc] << 8) + memory[pc + 1]
+  const inst = fetch()
   
   // decode & execute (i'm too lazy to have them do separately, feels like a waste? We'll see...)
   decodeAndExecute(inst)
+}
 
+// fetching from memory takes twoooo cycles, cos it's an 8-bit bus, 
+// but each instruction is 16-bits long. Neat-o. VLIW are lame.
+function fetch() {
+  iRegister = pc
+  const inst = (memory[iRegister] << 8) | memory[iRegister + 1]
   pc += 2
+
+  return inst
 }
 
 // aight we got a hex value now we need to look up what that means exactly
 function decodeAndExecute(inst) {
   // Many of the instructions follow the structure below, so to make my life simpler, I will calculate these values from the instruction
   // We are using bitmasking to get these values. If you don't quite get what's happening here, look up https://en.wikipedia.org/wiki/Mask_(computing)
+  logger.log('inst', inst)
   const nnn = inst & 0x0FFF // nnn or addr - A 12-bit value, the lowest 12 bits of the instruction
   const n = inst & 0x000F // n or nibble - A 4-bit value, the lowest 4 bits of the instruction  
-  const x = inst & 0x0F00 // x - A 4-bit value, the lower 4 bits of the high byte of the instructionction
-  const y = inst & 0x00F0 // y - A 4-bit value, the upper 4 bits of the low byte of the instru  
+  const x = (inst & 0x0F00) >> 8 // x - A 4-bit value, the lower 4 bits of the high byte of the instructionction
+  const y = (inst & 0x00F0) >> 4// y - A 4-bit value, the upper 4 bits of the low byte of the instru  
   const kk = inst & 0x00FF // kk or byte - An 8-bit value, the lowest 8 bits of the instruction
 
   // gonna split this into 'macro level opcodes' and 'micro level opcodes', as the instructions can be indexed by the 
   // highest byte first, and then after that we can decide what to do...
   const highByte = inst & 0xF000
   // We... could use a switch statement here, but that would be insane, right? How about a map instead?
-  const opcodes = {
+  const macroOpcodes = {
     0x0000: clearAndReturnOpcodes,
     0x1000: jump,
     0x2000: callSubroutine,
     0x3000: skipIfVxkk,
     0x4000: skipIfNotVxkk,
-    0x5000: skipIfVxVy
+    0x5000: skipIfVxVy,
+    0x6000: loadVxVal,
+    0x7000: addVxVal,
+    0x8000: settingFuncs,
+    0x9000: skipIfNotVxVy,
+    0xA000: loadIAddr,
+    0xB000: jumpV0Offset,
+    0xC000: setVxRandom,
+    0xD000: draw,
+    0xE000: skipKey,
+    0xF000: registerManipulation
   }
 
   logger.log('inst', inst)
+  const macroOpcode = macroOpcodes[highByte] ? macroOpcodes[highByte] : notImplemented  
 
-  opcodes[highByte](nnn)
+  macroOpcode({ nnn, n, x, y, kk }) // Yes, very ineffecient right now
+}
+
+function registerManipulation (nnn) {
+  logger.log('misc')
+  // Fx07 - LD Vx, DT
+  // Set Vx = delay timer value.
+
+  // The value of DT is placed into Vx.
+
+
+  // Fx0A - LD Vx, K
+  // Wait for a key press, store the value of the key in Vx.
+
+  // All execution stops until a key is pressed, then the value of that key is stored in Vx.
+
+
+  // Fx15 - LD DT, Vx
+  // Set delay timer = Vx.
+
+  // DT is set equal to the value of Vx.
+
+
+  // Fx18 - LD ST, Vx
+  // Set sound timer = Vx.
+
+  // ST is set equal to the value of Vx.
+
+
+  // Fx1E - ADD I, Vx
+  // Set I = I + Vx.
+
+  // The values of I and Vx are added, and the results are stored in I.
+
+
+  // Fx29 - LD F, Vx
+  // Set I = location of sprite for digit Vx.
+
+  // The value of I is set to the location for the hexadecimal sprite corresponding to the value of Vx. See section 2.4, Display, for more information on the Chip-8 hexadecimal font.
+
+
+  // Fx33 - LD B, Vx
+  // Store BCD representation of Vx in memory locations I, I+1, and I+2.
+
+  // The interpreter takes the decimal value of Vx, and places the hundreds digit in memory at location in I, the tens digit at location I+1, and the ones digit at location I+2.
+
+
+  // Fx55 - LD [I], Vx
+  // Store registers V0 through Vx in memory starting at location I.
+
+  // The interpreter copies the values of registers V0 through Vx into memory, starting at the address in I.
+
+
+  // Fx65 - LD Vx, [I]
+  // Read registers V0 through Vx from memory starting at location I.
+
+  // The interpreter reads values from memory starting at location I into registers V0 through Vx.
+}
+
+function skipKey (nnn) {
+  logger.log('skipKey')
+  // Ex9E - SKP Vx
+  // Skip next instruction if key with the value of Vx is pressed.
+  
+  // Checks the keyboard, and if the key corresponding to the value of Vx is currently in the down position, PC is increased by 2.
+  
+  
+  // ExA1 - SKNP Vx
+  // Skip next instruction if key with the value of Vx is not pressed.
+  
+  // Checks the keyboard, and if the key corresponding to the value of Vx is currently in the up position, PC is increased by 2.
+}
+
+// Dxyn - DRW Vx, Vy, nibble
+// Display n-byte sprite starting at memory location I at (Vx, Vy), set VF = collision.
+
+// The interpreter reads n bytes from memory, starting at the address stored in I. 
+// These bytes are then displayed as sprites on screen at coordinates (Vx, Vy). 
+// Sprites are XORed onto the existing screen. 
+// If this causes any pixels to be erased, VF is set to 1, otherwise it is set to 0. 
+// If the sprite is positioned so part of it is outside the coordinates of the display, 
+// it wraps around to the opposite side of the screen. See instruction 8xy3 
+// for more information on XOR, and section 2.4, Display, for more information on the Chip-8 
+// screen and sprites.
+function draw (inst) {
+  logger.log('draw')
+
+  let x = vRegisters[inst.x]
+  let y = vRegisters[inst.y]
+  vRegisters[0xF] = 0
+
+  for (var i = 0; i < inst.n; i += 1) {
+    if (y + i > DISPLAY_HEIGHT - 1) y = 0
+
+    const line = memory[iRegister]
+    iRegister += 1
+
+    for (var j = 0; j < WORD_SIZE; j += 1) {
+      if (x + j > DISPLAY_WIDTH - 1) x = 0
+
+      const bitmask = 0b00000001 << j
+      const pixel = (line & bitmask) >> j
+
+      console.log('---')
+      console.log(inst)
+      console.log(`j: ${j}`)
+      console.log(`line: ${line.toString(2)}`)
+      console.log(`bitmask: ${bitmask.toString(2)}`)
+      console.log(`pixel: ${pixel.toString(2)}`)
+      console.log('---')
+
+      console.log(x + j)
+      console.log(y + i)
+      
+      const currentPixel = display[x + j][y + i]
+      const newPixel = currentPixel ^ pixel
+      display[x + j][y + i] = newPixel
+
+      console.log(`${currentPixel} ^ ${pixel} = ${newPixel}`)
+      console.log(newPixel)
+
+      vRegisters[0xF] = newPixel
+    }
+  }
+}
+
+function setVxRandom (nnn) {
+  logger.log('setVxRandom')
+  // Cxkk - RND Vx, byte
+  // Set Vx = random byte AND kk.
+  
+  // The interpreter generates a random number from 0 to 255, which is then ANDed with the value kk. The results are stored in Vx. See instruction 8xy2 for more information on AND.
+}
+
+function jumpV0Offset (nnn) {
+  logger.log('jumpV0Offset')
+  // Bnnn - JP V0, addr
+  // Jump to location nnn + V0.
+
+  // The program counter is set to nnn plus the value of V0.
+}
+
+// Annn - LD I, addr
+// Set I = nnn.
+
+// The value of register I is set to nnn.
+function loadIAddr (inst) {
+  logger.log('loadIAddr')
+  iRegister = inst.nnn
+}
+
+function skipIfNotVxVy (nnn) {
+  logger.log('skipIfNotVxVy')
+  // 9xy0 - SNE Vx, Vy
+  // Skip next instruction if Vx != Vy.
+
+  // The values of Vx and Vy are compared, and if they are not equal, the program counter is increased by 2.
+}
+
+// 6xkk - LD Vx, byte
+// Set Vx = kk.
+
+// The interpreter puts the value kk into register Vx.
+function loadVxVal (inst) {
+  logger.log('loadVxVal')
+  vRegisters[inst.x] = inst.kk
+}
+
+function addVxVal (nnn) {
+  logger.log('addVxVal')
+  // Set Vx = Vx + kk.
+
+  // Adds the value kk to the value of register Vx, then stores the result in Vx. 
+}
+
+function settingFuncs (nnn) {
+  // 8xy0 - LD Vx, Vy
+  // Set Vx = Vy.
+  
+  // Stores the value of register Vy in register Vx.
+  
+  
+  // 8xy1 - OR Vx, Vy
+  // Set Vx = Vx OR Vy.
+  
+  // Performs a bitwise OR on the values of Vx and Vy, then stores the result in Vx. A bitwise OR compares the corrseponding bits from two values, and if either bit is 1, then the same bit in the result is also 1. Otherwise, it is 0.
+  
+  
+  // 8xy2 - AND Vx, Vy
+  // Set Vx = Vx AND Vy.
+  
+  // Performs a bitwise AND on the values of Vx and Vy, then stores the result in Vx. A bitwise AND compares the corrseponding bits from two values, and if both bits are 1, then the same bit in the result is also 1. Otherwise, it is 0.
+  
+  
+  // 8xy3 - XOR Vx, Vy
+  // Set Vx = Vx XOR Vy.
+  
+  // Performs a bitwise exclusive OR on the values of Vx and Vy, then stores the result in Vx. An exclusive OR compares the corrseponding bits from two values, and if the bits are not both the same, then the corresponding bit in the result is set to 1. Otherwise, it is 0.
+  
+  
+  // 8xy4 - ADD Vx, Vy
+  // Set Vx = Vx + Vy, set VF = carry.
+  
+  // The values of Vx and Vy are added together. If the result is greater than 8 bits (i.e., > 255,) VF is set to 1, otherwise 0. Only the lowest 8 bits of the result are kept, and stored in Vx.
+  
+  
+  // 8xy5 - SUB Vx, Vy
+  // Set Vx = Vx - Vy, set VF = NOT borrow.
+  
+  // If Vx > Vy, then VF is set to 1, otherwise 0. Then Vy is subtracted from Vx, and the results stored in Vx.
+  
+  
+  // 8xy6 - SHR Vx {, Vy}
+  // Set Vx = Vx SHR 1.
+  
+  // If the least-significant bit of Vx is 1, then VF is set to 1, otherwise 0. Then Vx is divided by 2.
+  
+  
+  // 8xy7 - SUBN Vx, Vy
+  // Set Vx = Vy - Vx, set VF = NOT borrow.
+  
+  // If Vy > Vx, then VF is set to 1, otherwise 0. Then Vx is subtracted from Vy, and the results stored in Vx.
+  
+  
+  // 8xyE - SHL Vx {, Vy}
+  // Set Vx = Vx SHL 1.
+  
+  // If the most-significant bit of Vx is 1, then VF is set to 1, otherwise to 0. Then Vx is multiplied by 2.
+}
+
+function notImplemented (nnn) {
+  logger.log('function not implemented')
 }
 
 function clearAndReturnOpcodes (nnn) {
--- src/main.js
+++ src/main.js
@@ -1,11 +1,55 @@
 import chip8 from './chip-8'
 
-chip8.memory[0x200] = 0x00
-chip8.memory[0x201] = 0xE0
+function loadMemory (file) {
+  const array = new Uint8Array(file)
 
-chip8.cycle()
+  for (let i = 0; i < array.length; i += 1) {
+    chip8.memory[0x200 + i] = array[i]
+  }
 
-chip8.memory[0x202] = 0x50
-chip8.memory[0x203] = 0x00
+  loop()
+}
 
-chip8.cycle()
+function loop () {
+  for (let i = 0; i < 10; i += 1) {
+    chip8.cycle()
+    drawCanvas()
+  }
+
+  console.log('done looping')
+}
+
+function drawCanvas () {
+  const canvas = document.getElementById('canvas')
+  const context = canvas.getContext('2d')
+
+  for (var x = 0; x < chip8.display.length; x += 1) {
+    for (var y = 0; y < chip8.display[0].length; y += 1) {
+      const pixel = chip8.display[x][y]
+      context.fillStyle = pixel ? '#fff' : '#000'
+      context.fillRect(x * 10, y * 10, 10, 10)
+    }
+  }
+}
+
+function dumpMemory () {
+  for (var i = 0; i < chip8.memory.length; i += 1) {
+    console.log(chip8.memory[i])
+  }
+}
+
+function readSingleFile (event) {
+  const canvas = document.getElementById('canvas')
+  const context = canvas.getContext('2d')
+  context.fillRect(0, 0, 640, 320, '#000')
+
+  var filename = event.target.files[0]
+
+  if (!filename) return
+
+  const reader = new FileReader()
+  reader.onload = file => loadMemory(file.target.result)  
+  reader.readAsArrayBuffer(filename)
+}
+
+document.getElementById('file-input').addEventListener('change', readSingleFile, false)
