bcc130d6858194ec174dab61d2ff3aa0e065c49b
adamjc <adamjc86@gmail.com>
2019-02-13 22:55:20 +0000

    Add sourcemapping and copy index.html to dist/

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,24 @@
 const path = require('path')
+const copyWebpackPlugin = require('copy-webpack-plugin')
 
 module.exports = {
+  mode: 'development',
   entry: './src/main.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: './main.js'
-  }
+  },
+  plugins: [
+    copyWebpackPlugin([{
+      from: path.resolve(__dirname, 'src/index.html'),
+      to: path.resolve(__dirname, 'dist/index.html')
+    }])
+  ],
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 9000
+  },
+  devtool: 'eval-source-map',
+  watch: true
 }
