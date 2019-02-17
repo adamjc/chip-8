2fa1ae6da6c737475e43b6ab6c6680106faec94d
adamjc <adamjc86@gmail.com>
2019-02-17 11:14:42 +0000

    Clean up comments

--- src/chip-8.js
+++ src/chip-8.js
@@ -40,7 +40,7 @@ const fonts = [
 
 memory.splice(0, fonts.length, ...fonts)
 
-// There are 2 timers, a delay timer and a sound timer, both decrease to 0 at a rate of 60Hz, once at 0 they stay there.
+// There are 2 timers, a delay timer and a sound timer, both decrease to 0 at a rate of 60Hz, once at 0 they stay there
 let delayTimer
 
 // When the sound timer hits 0, a monotone sound is played.
@@ -54,8 +54,8 @@ let sp = 0
 // The stack. Documentation says it's 16 deep, but apparently only 10 are ever used? I'll stick with 16 just in case...
 let stack = new Array(16)
 
-// It utilises a 64x32 pixel display... we will get the chip-8 to write to these values, and in our `emulator` code, we will write these
-// values to a screen. Simple! (should be!).
+// It utilises a 64x32 pixel display... we will get the chip-8 to write to these values, and in our `emulator` code, 
+// we will write these values to a screen. Simple! (should be!).
 const DISPLAY_WIDTH = 64
 const DISPLAY_HEIGHT = 32
 let display = new Array(DISPLAY_WIDTH).fill().map(_ => new Array(DISPLAY_HEIGHT).fill(0))
@@ -79,8 +79,9 @@ function fetch() {
 
 // aight we got a hex value now we need to look up what that means exactly
 function decodeAndExecute(inst) {
-  // Many of the instructions follow the structure below, so to make my life simpler, I will calculate these values from the instruction
-  // We are using bitmasking to get these values. If you don't quite get what's happening here, look up https://en.wikipedia.org/wiki/Mask_(computing)
+  // Many of the instructions follow the structure below, so to make my life simpler, I will calculate these values 
+  // from the instruction We are using bitmasking to get these values. If you don't quite get what's happening here, 
+  // look up https://en.wikipedia.org/wiki/Mask_(computing)
   logger.log('-----')
   logger.log('inst', inst)
   const nnn = inst & 0x0FFF // nnn or addr - A 12-bit value, the lowest 12 bits of the instruction
@@ -133,9 +134,9 @@ function clearAndReturnOpcodes (inst) {
   }
 
   // 00EE - RET
-  // The interpreter sets the program counter to the address at the top of the stack, then subtracts 1 from the stack pointer.
-  // We're doing it the other way around (subtract, THEN set), because otherwise our 'stack's' (which is an array) first element
-  // is never set (stack[0] will never be used)
+  // The interpreter sets the program counter to the address at the top of the stack, then subtracts 1 from the stack 
+  // pointer. We're doing it the other way around (subtract, THEN set), because otherwise our 'stack's' (which is an 
+  // array) first element is never set (stack[0] will never be used)
   function returnFromSub () {
     logger.log('returnFromSub')
     
@@ -152,8 +153,8 @@ function jump (nnn) {
 
 // 2nnn - CALL addr
 // Call subroutine at nnn.
-// The interpreter increments the stack pointer, then puts the current PC on the top of the stack. The PC is then set to nnn.
-// We're doing this the other way around (set, then increment), see 00EE for more info.
+// The interpreter increments the stack pointer, then puts the current PC on the top of the stack. The PC is then set 
+// to nnn. We're doing this the other way around (set, then increment), see 00EE for more info.
 function callSubroutine (inst) {
   logger.log('callSubroutine')
 
@@ -166,28 +167,25 @@ function callSubroutine (inst) {
   logger.log('pc', pc)
 }
 
-//  3xkk - SE Vx, byte
+// 3xkk - SE Vx, byte
+// Skip next instruction if Vx = kk.
+// The interpreter compares register Vx to kk, and if they are equal, increments the program counter by 2.
 function skipIfVxkk (nnn) {
   logger.log('skipIfVxkk')
-  //   Skip next instruction if Vx = kk.
-
-  // The interpreter compares register Vx to kk, and if they are equal, increments the program counter by 2.
 }
 
 // 4xkk - SNE Vx, byte
+// Skip next instruction if Vx != kk.
+// The interpreter compares register Vx to kk, and if they are not equal, increments the program counter by 2.
 function skipIfNotVxkk (nnn) {
   logger.log('skipIfNotVxkk')
-  // Skip next instruction if Vx != kk.
-
-  // The interpreter compares register Vx to kk, and if they are not equal, increments the program counter by 2.
 }
 
 // 5xy0 - SE Vx, Vy
+// Skip next instruction if Vx = Vy.
+// The interpreter compares register Vx to register Vy, and if they are equal, increments the program counter by 2.
 function skipIfVxVy (nnn) {
   logger.log('skipIfVxVy')
-  // Skip next instruction if Vx = Vy.
-
-  // The interpreter compares register Vx to register Vy, and if they are equal, increments the program counter by 2.
 }
 
 // 6xkk - LD Vx, byte
@@ -217,59 +215,56 @@ function settingFuncs (nnn) {
   
   // 8xy1 - OR Vx, Vy
   // Set Vx = Vx OR Vy.
-  
-  // Performs a bitwise OR on the values of Vx and Vy, then stores the result in Vx. A bitwise OR compares the corrseponding bits from two values, and if either bit is 1, then the same bit in the result is also 1. Otherwise, it is 0.
+  // Performs a bitwise OR on the values of Vx and Vy, then stores the result in Vx. A bitwise OR compares the 
+  // corrseponding bits from two values, and if either bit is 1, then the same bit in the result is also 1. Otherwise, 
+  // it is 0.
   
   
   // 8xy2 - AND Vx, Vy
   // Set Vx = Vx AND Vy.
-  
-  // Performs a bitwise AND on the values of Vx and Vy, then stores the result in Vx. A bitwise AND compares the corrseponding bits from two values, and if both bits are 1, then the same bit in the result is also 1. Otherwise, it is 0.
+  // Performs a bitwise AND on the values of Vx and Vy, then stores the result in Vx. A bitwise AND compares the 
+  // corrseponding bits from two values, and if both bits are 1, then the same bit in the result is also 1. Otherwise, 
+  // it is 0.
   
   
   // 8xy3 - XOR Vx, Vy
   // Set Vx = Vx XOR Vy.
-  
-  // Performs a bitwise exclusive OR on the values of Vx and Vy, then stores the result in Vx. An exclusive OR compares the corrseponding bits from two values, and if the bits are not both the same, then the corresponding bit in the result is set to 1. Otherwise, it is 0.
+  // Performs a bitwise exclusive OR on the values of Vx and Vy, then stores the result in Vx. An exclusive OR compares 
+  // the corrseponding bits from two values, and if the bits are not both the same, then the corresponding bit in the 
+  // result is set to 1. Otherwise, it is 0.
   
   
   // 8xy4 - ADD Vx, Vy
   // Set Vx = Vx + Vy, set VF = carry.
-  
-  // The values of Vx and Vy are added together. If the result is greater than 8 bits (i.e., > 255,) VF is set to 1, otherwise 0. Only the lowest 8 bits of the result are kept, and stored in Vx.
+  // The values of Vx and Vy are added together. If the result is greater than 8 bits (i.e., > 255,) VF is set to 1, 
+  // otherwise 0. Only the lowest 8 bits of the result are kept, and stored in Vx.
   
   
   // 8xy5 - SUB Vx, Vy
   // Set Vx = Vx - Vy, set VF = NOT borrow.
-  
   // If Vx > Vy, then VF is set to 1, otherwise 0. Then Vy is subtracted from Vx, and the results stored in Vx.
   
   
   // 8xy6 - SHR Vx {, Vy}
   // Set Vx = Vx SHR 1.
-  
   // If the least-significant bit of Vx is 1, then VF is set to 1, otherwise 0. Then Vx is divided by 2.
   
   
   // 8xy7 - SUBN Vx, Vy
   // Set Vx = Vy - Vx, set VF = NOT borrow.
-  
   // If Vy > Vx, then VF is set to 1, otherwise 0. Then Vx is subtracted from Vy, and the results stored in Vx.
   
   
   // 8xyE - SHL Vx {, Vy}
   // Set Vx = Vx SHL 1.
-  
   // If the most-significant bit of Vx is 1, then VF is set to 1, otherwise to 0. Then Vx is multiplied by 2.
 }
 
 // 9xy0 - SNE Vx, Vy
+// Skip next instruction if Vx != Vy.
+// The values of Vx and Vy are compared, and if they are not equal, the program counter is increased by 2.
 function skipIfNotVxVy (nnn) {
   logger.log('skipIfNotVxVy')
-  
-  // Skip next instruction if Vx != Vy.
-
-  // The values of Vx and Vy are compared, and if they are not equal, the program counter is increased by 2.
 }
 
 // 0xA000
@@ -283,7 +278,6 @@ function loadIAddr (inst) {
 
 // Bnnn - JP V0, addr
 // Jump to location nnn + V0.
-
 // The program counter is set to nnn plus the value of V0.
 function jumpV0Offset (nnn) {
   logger.log('jumpV0Offset')
@@ -291,23 +285,20 @@ function jumpV0Offset (nnn) {
 
 // Cxkk - RND Vx, byte
 // Set Vx = random byte AND kk.
-
-// The interpreter generates a random number from 0 to 255, which is then ANDed with the value kk. The results are stored in Vx. See instruction 8xy2 for more information on AND.
+// The interpreter generates a random number from 0 to 255, which is then ANDed with the value kk. The results are 
+// stored in Vx. See instruction 8xy2 for more information on AND.
 function setVxRandom (nnn) {
   logger.log('setVxRandom')
 }
 
 // Dxyn - DRW Vx, Vy, nibble
 // Display n-byte sprite starting at memory location I at (Vx, Vy), set VF = collision.
-
 // The interpreter reads n bytes from memory, starting at the address stored in I. 
 // These bytes are then displayed as sprites on screen at coordinates (Vx, Vy). 
 // Sprites are XORed onto the existing screen. 
 // If this causes any pixels to be erased, VF is set to 1, otherwise it is set to 0. 
-// If the sprite is positioned so part of it is outside the coordinates of the display, 
-// it wraps around to the opposite side of the screen. See instruction 8xy3 
-// for more information on XOR, and section 2.4, Display, for more information on the Chip-8 
-// screen and sprites.
+// If the sprite is positioned so part of it is outside the coordinates of the display, i t wraps around to the 
+// opposite side of the screen.
 function draw (inst) {
   let x = vRegisters[inst.x]
   let y = vRegisters[inst.y]
@@ -337,14 +328,14 @@ function draw (inst) {
 
 // Ex9E - SKP Vx
 // Skip next instruction if key with the value of Vx is pressed.
-
-// Checks the keyboard, and if the key corresponding to the value of Vx is currently in the down position, PC is increased by 2.
+// Checks the keyboard, and if the key corresponding to the value of Vx is currently in the down position, PC is 
+// increased by 2.
 
 
 // ExA1 - SKNP Vx
 // Skip next instruction if key with the value of Vx is not pressed.
-
-// Checks the keyboard, and if the key corresponding to the value of Vx is currently in the up position, PC is increased by 2.
+// Checks the keyboard, and if the key corresponding to the value of Vx is currently in the up position, PC is 
+// increased by 2.
 function skipKey (nnn) {
   logger.log('skipKey')
 }
@@ -361,7 +352,7 @@ function registerManipulation (inst) {
 
   // Fx29 - LD F, Vx
   // Set I = location of sprite for digit Vx.
-  // The value of I is set to the location for the hexadecimal sprite corresponding to the value of Vx. See section 2.4, Display, for more information on the Chip-8 hexadecimal font.
+  // The value of I is set to the location for the hexadecimal sprite corresponding to the value of Vx.
   function loadIVx () {
     logger.log('loadIVx')
     iRegister = vRegisters[inst.x] * 5
@@ -393,39 +384,29 @@ function registerManipulation (inst) {
   }
 
   return microOpCodes[inst.kk]()
+  
   // Fx07 - LD Vx, DT
   // Set Vx = delay timer value.
-
   // The value of DT is placed into Vx.
-
 
   // Fx0A - LD Vx, K
   // Wait for a key press, store the value of the key in Vx.
-
   // All execution stops until a key is pressed, then the value of that key is stored in Vx.
-
 
   // Fx15 - LD DT, Vx
   // Set delay timer = Vx.
-
   // DT is set equal to the value of Vx.
-
 
   // Fx18 - LD ST, Vx
   // Set sound timer = Vx.
-
   // ST is set equal to the value of Vx.
-
 
   // Fx1E - ADD I, Vx
   // Set I = I + Vx.
-
   // The values of I and Vx are added, and the results are stored in I.
-
 
   // Fx55 - LD [I], Vx
   // Store registers V0 through Vx in memory starting at location I.
-
   // The interpreter copies the values of registers V0 through Vx into memory, starting at the address in I.
 }
 
